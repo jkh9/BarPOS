5fadd4c79d10f0bd7765aa5f4d460fad380237fc
MOISEX-A-PINXO\jkh9L <jkh91234@gmail.com>
2018-05-14 17:57:25 +0200

    no message

--- BarPOS Server/AdminScreen.Designer.cs
+++ BarPOS Server/AdminScreen.Designer.cs
@@ -31,6 +31,9 @@
             this.btnProducts = new System.Windows.Forms.Button();
             this.btnUsers = new System.Windows.Forms.Button();
             this.btnAccount = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnProducts
@@ -39,7 +42,7 @@
             this.btnProducts.FlatAppearance.BorderSize = 0;
             this.btnProducts.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnProducts.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProducts.Location = new System.Drawing.Point(75, 342);
+            this.btnProducts.Location = new System.Drawing.Point(172, 399);
             this.btnProducts.Name = "btnProducts";
             this.btnProducts.Size = new System.Drawing.Size(323, 93);
             this.btnProducts.TabIndex = 1;
@@ -52,7 +55,7 @@
             this.btnUsers.FlatAppearance.BorderSize = 0;
             this.btnUsers.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnUsers.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUsers.Location = new System.Drawing.Point(75, 190);
+            this.btnUsers.Location = new System.Drawing.Point(172, 247);
             this.btnUsers.Name = "btnUsers";
             this.btnUsers.Size = new System.Drawing.Size(323, 93);
             this.btnUsers.TabIndex = 2;
@@ -65,25 +68,49 @@
             this.btnAccount.FlatAppearance.BorderSize = 0;
             this.btnAccount.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnAccount.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAccount.Location = new System.Drawing.Point(75, 38);
+            this.btnAccount.Location = new System.Drawing.Point(172, 95);
             this.btnAccount.Name = "btnAccount";
             this.btnAccount.Size = new System.Drawing.Size(323, 93);
             this.btnAccount.TabIndex = 3;
             this.btnAccount.Text = "Account";
             this.btnAccount.UseVisualStyleBackColor = false;
             // 
-            // ConfigurationScreen
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 50);
+            this.panel1.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Firebrick;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(650, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // AdminScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.ClientSize = new System.Drawing.Size(700, 700);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnProducts);
             this.Controls.Add(this.btnUsers);
             this.Controls.Add(this.btnAccount);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "ConfigurationScreen";
+            this.Name = "AdminScreen";
             this.Text = "ConfigurationScreen";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -93,5 +120,7 @@
         private System.Windows.Forms.Button btnProducts;
         private System.Windows.Forms.Button btnUsers;
         private System.Windows.Forms.Button btnAccount;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
