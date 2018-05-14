58a8aa4caf9c593b5edfc7eed89c96c42fe3cf8a
MOISEX-A-PINXO\jkh9L <jkh91234@gmail.com>
2018-05-14 18:49:25 +0200

    Added the main skeleton

--- BarPOS Client/TablesScreen.cs
+++ BarPOS Client/TablesScreen.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿// Bar POS, class TableScreen
+
+// Versiones: 
+// V0.01 14-May-2018 Moisés: Basic skeleton
+
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BarPOS
 {
     public partial class TableScreen : Form
     {
+        TableList Tables { get;}
+        Image Image { get; set; }
+
         public TableScreen()
         {
+            Image = this.picTables.Image;
             InitializeComponent();
         }
     }
