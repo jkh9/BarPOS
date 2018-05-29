8052f29e1729d7edfae325c6f1f99da2d487d891
MOISEX-A-PINXO\jkh9L <jkh91234@gmail.com>
2018-05-29 20:55:21 +0200

    New class to inherit

---
+++ BarPOS/BarPOS Server/Management.cs
@@ -0,0 +1,13 @@
+﻿// Bar POS, class UserManagmentClass
+
+// Versiones: 
+// V0.01 22-May-2018 Moisés: Class implemented
+
+namespace BarPOS
+{
+    public class Management
+    {
+        public int Index { get; set; }
+        public bool DrawFounds { get; set; }
+    }
+}
